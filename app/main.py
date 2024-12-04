d705da35d72fcdb499df194a9f770ed5e76b1b46
tapo44ek <viktooooor2001@gmail.com>
2024-12-04 12:00:51 +0300

    /dinogame/ path

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from fastapi.staticfiles import StaticFiles
 from app.api import routes_game, routes_scores
 from app.core.database import Base, engine
 
-app = FastAPI(title="FastAPI Dino Game")
+app = FastAPI(title="FastAPI Dino Game", root_path="/dinogame")
 
 # Создание таблиц базы данных
 Base.metadata.create_all(bind=engine)
 
 # Подключение статических файлов
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/dinogame/static", StaticFiles(directory="static"), name="static")
 
 # Подключение маршрутов
 app.include_router(routes_game.router, tags=["Game"])
